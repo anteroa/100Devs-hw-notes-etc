c03ec45d766a52ea85ecd7053073aa37aa5d0b3d
Antero <anteroavalos2508@gmail.com>
2022-03-24 19:10:27 -0700

    ...

--- class materials/class22-materials/objects-lost-galaxy/js/main.js
+++ class materials/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,37 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {}
+
+mouse.color = "black"
+mouse.weight = "20 grams"
+mouse.brand = "Razer"
+mouse.wireless = false
+
+mouse.leftClick = function(){
+    console.log("left click")
+}
+
+mouse.rightClick = function(){
+    console.log("right click")
+}
+
+mouse.scroll = function(){
+    console.log("JUMP JUMP JUMP")
+}
+
+
+//Constructor for the above object, much better...
+/**
+function makeCar(carMake, carModel, carColor, numOfDoors){
+    this.make = carMake, 
+    this.model = carModel,
+    this.color = carColor,
+    this.doors = numOfDoors
+
+    this.honk = function(){
+        alert('BEEP BEEP FUCKER')
+    }
+    this.lock = function(){
+        alert(`Locked ${this.doors} doors!`)
+    }
+}
+ */
