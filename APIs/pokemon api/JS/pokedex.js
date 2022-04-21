a680eaba486af1f0fa87509694367364f2bd674e
Antero <anteroavalos2508@gmail.com>
2022-04-21 06:18:12 -0700

    ...

--- APIs/pokemon api/JS/pokedex.js
+++ APIs/pokemon api/JS/pokedex.js
@@ -1,52 +1,86 @@
+//common query selector
+const q_s = (sct)=> document.querySelector(sct);
+
+document.querySelector('.search_button').addEventListener("click", getPokemon);
+
 let pokeName = document.querySelector('#pokemon_entry').value;
 
-Promise.all([
-	fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`),
-	fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}/`)
-]).then(function (responses) {
-	// Get a JSON object from each of the responses
-	return Promise.all(responses.map(function (response) {
-		return response.json();
-	}));
-}).then(function (data) {
-	// Log the data to the console
-	// You would do something with both sets of data here
-	console.log(data);
-
-	//pokemon name
-	let name = capFirstLetter(data[0].name);
-	document.querySelector('#name').textContent = name
-
-	//pokemon type
-	document.querySelector('#type').textContent = data[0].types[0].type.name
-
-	//pokemon stats
+function getPokemon(){
+	Promise.all([
+		fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`),
+		fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}/`)
+	]).then(function (responses) {
+		// Get a JSON object from each of the responses
+		return Promise.all(responses.map(function (response) {
+			return response.json();
+		}));
+	}).then(function (data) {
+		// Log the data to the console
+		// You would do something with both sets of data here
+		console.log(data);
 	
 
-	//get pokemon image from responses
-	let sprite = data[0].sprites.other["official-artwork"].front_default;
-	document.querySelector('.screen').innerHTML = 
-	`<img id="sprite" src="${sprite}"alt="${data.name}">`
+		//get pokemon image 
+		let sprite = data[0].sprites.other["official-artwork"].front_default;
+		q_s('.screen').innerHTML = 
+		`<img id="sprite" src="${sprite}"alt="${data.name}">`
 
-	//get pokemon description
-	let description = data[1].flavor_text_entries[0].flavor_text;
-	document.querySelector('.poke_desc').textContent = description;
+		//pokemon names ( english and japanese )
+		let engName = capFirstLetter(data[0].name);
+		let japName = capFirstLetter(data[1].names[9].name);
+		let nameStr = engName + " / " + japName
+		q_s('#name').textContent = nameStr
+	
+		//pokemon type
+		let typeStr = 
+		engName + " is a " + data[0].types[0].type.name + " type pokemon."
+		q_s('#type').textContent = typeStr
+	
+		//pokemon stats
+		let hpVal = data[0].stats[0].base_stat;
+		let spAttVal = data[0].stats[1].base_stat;
+		let attVal = data[0].stats[2].base_stat;
+		let spcDfVal = data[0].stats[3].base_stat;
+		let defVal = data[0].stats[4].base_stat;
+		let speedVal = data[0].stats[5].base_stat;
+		q_s('#hp').textContent = hpVal
+		q_s('#spcAtt').textContent = spAttVal
+		q_s('#att').textContent = attVal
+		q_s('#spcDef').textContent = spcDfVal
+		q_s('#def').textContent = defVal
+		q_s('#speed').textContent = speedVal
+	
+		
+	
+		//get pokemon description
+		let description = data[1].flavor_text_entries[0].flavor_text;
+		document.querySelector('.poke_desc').textContent = description;
+	
+		//Evolution chain sprites
+		let chainURL = data[1].evolution_chain.url
+		getEvoChain(chainURL)
 
-	//Evo chain images - needs to be fixed
-	document.querySelector('#evo_container').innerHTML = 
-	`<img class="evo_node" src="${data[0].sprites.other["official-artwork"].front_default}"
-	alt="${data[0].name}">
-	<img class="evo_node" src="${data[0].sprites.other["official-artwork"].front_default}"
-	alt="${data[0].name}">
-	<img class="evo_node" src="${data[0].sprites.other["official-artwork"].front_default}"
-	alt="${data[0].name}">`
+		let evolvedFrom = data[1].evolves_from_species.name
+		let current = sprite
+		
+		//calls api for sprite url and adds it to DOM
+		getSprite(evolvedFrom, "evoA") 
+		
 
+		q_s('#evo_container').innerHTML = 
+		`<img id="evoA" class="evo_node" src="" alt="">
 
-}).catch(function (error) {
-	// if there's an error, log it
-	console.log(error);
-});				
-
+		<img id="evoB" class="evo_node" src="${current}" alt="">
+		
+		<img id="evoC" class="evo_node" src="" alt="$">`
+	
+	
+	}).catch(function (error) {
+		// if there's an error, log it
+		console.log(error);
+	});				
+	
+}
 
 //Capitalize fir letter of a string, used for data formatting
 function capFirstLetter(str){
@@ -61,4 +95,25 @@ function capFirstLetter(str){
 	}
 
 	return resStr;
+}
+
+function getSprite(pokemon, imgId){
+	fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+  		.then(response => response.json())
+  		.then(data => 
+				q_s(`#${imgId}`).src = data.sprites.other["official-artwork"].front_default
+			)
+  		.catch(error => console.log('error', error));
+}
+
+//gets a json response with the evolution chain data
+//the parameter should be the resulting evo chain url
+//from the initial API request
+function getEvoChain(url){
+	fetch(`${url}`)
+  		.then(response => response.json())
+  		.then(data => 
+				console.log(data)
+			)
+  		.catch(error => console.log('error', error));
 }
